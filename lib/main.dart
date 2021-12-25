215e08325bb308f36cc5d339fea49f4a80c450d4
mensur056 <mansur.sarxanov@gmail.com>
2021-12-25 22:48:19 +0400

    test

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp(debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
